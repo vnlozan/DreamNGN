f87d60a20d79ecc6e1561b309dae17dcfcf3161a
Vladlen Lozan <vnlozan@gmail.com>
2021-02-09 01:33:12 +0200

    Adding Events

---
+++ DreamNGN/src/DreamNGN/Events/KeyEvent.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Event.h"
+
+#include <sstream>
+
+namespace DreamNGN {
+	//abstract class
+	class DN_API KeyEvent : public Event {
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+		EVENT_CLASS_CATEGORY( EventCategoryKeyboard | EventCategoryInput )
+	protected:
+		KeyEvent( int keycode ) : m_KeyCode{ keycode } {}
+		int m_KeyCode;
+	};
+
+	class DN_API KeyPressedEvent : public KeyEvent {
+	public:
+		KeyPressedEvent( int keycode, int repeatCount ) : 
+			KeyEvent{ keycode }, m_RepeatCount{ repeatCount } {}
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyPressedEvent: " << m_KeyCode << "(" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE( keyPressed )
+
+	private:
+		int m_RepeatCount;
+	};
+}
